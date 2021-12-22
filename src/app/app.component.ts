a852dd5069636fabebc8b09882e23ba34f00b5b7
edwarddeen <eaadcom@gmail.com>
2021-12-22 23:10:32 +0100

    connection with backend WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {ApiService} from "./shared/services/api.service";
+import {Account} from "./models/account"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'KookShopFront';
+  title = 'Ed\'s Shop';
+  account = {} as Account;
+  email = '';
+
+  public constructor(private titleService: Title, private apiService: ApiService) {
+    this.setTitle(this.title);
+    this.testConnections();
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+  public testConnections(){
+    this.apiService.get('accounts/1').subscribe(response =>
+    {
+      this.account = response
+      this.email = this.account.email!;
+    });
+  }
 }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '14f590fe52dece70a542bc3465821f5aae1c0795',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14f590fe52dece70a542bc3465821f5aae1c0795',
+    ),
+  ),
+);
